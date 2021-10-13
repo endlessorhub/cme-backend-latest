2201169fd6dc1a11425b11f628a05c83b780a259
Florian Eyzat <florianeyzat1993@gmail.com>
2021-10-14 00:34:35 +0200

    Combat system update

---
+++ apps/battles-manager/src/utils/attackReport.ts
@@ -0,0 +1,112 @@
+import * as _ from 'lodash';
+
+import { StakeholderStatus, unitInfo, unitInfoByType } from '../types';
+
+const computeCasualtiesForTeam = (
+  stakeholderUnitsInfoByType: unitInfoByType,
+  casualtiesRatio: number,
+) => {
+  return _.mapValues(stakeholderUnitsInfoByType, (unitsInfo) => {
+    return {
+      ...unitsInfo,
+      count: Math.round(unitsInfo.count * casualtiesRatio),
+    };
+  });
+};
+
+const computePointsForTeam = (
+  stakeholderUnitsInfoByType: unitInfoByType,
+  stakeholderStatus: StakeholderStatus,
+  unitsInfo: unitInfo[],
+): number => {
+  return _.sumBy(unitsInfo, (unitInfo) => {
+    if (stakeholderUnitsInfoByType[unitInfo.unitTypeName] === undefined) {
+      return 0;
+    }
+    return (
+      stakeholderUnitsInfoByType[unitInfo.unitTypeName].count *
+      (stakeholderStatus === StakeholderStatus.ATTACKER
+        ? unitInfo.characteristics.damage
+        : unitInfo.characteristics.defense +
+          unitInfo.characteristics.pierce_defense)
+    );
+  });
+};
+
+export const generateAttackReport = (
+  attackId: number,
+  travelTime: number,
+  attackerVillageId: number,
+  attackerUnitsInfoByType: Record<string, any>,
+  defenderVillageId: number,
+  defenderUnitsInfoByType: Record<string, any>,
+  unitsInfo: unitInfo[],
+) => {
+  // Step 1: compute logical points for each team and find out who wins
+  const attackerPoints = computePointsForTeam(
+    attackerUnitsInfoByType,
+    StakeholderStatus.ATTACKER,
+    unitsInfo,
+  );
+  const defenderPoints = computePointsForTeam(
+    defenderUnitsInfoByType,
+    StakeholderStatus.DEFENDER,
+    unitsInfo,
+  );
+
+  const attackerWon = attackerPoints > defenderPoints;
+  const nobodyWon = attackerPoints === defenderPoints;
+
+  // Step 2: set a base ratio for casualties
+  let casualtiesBase: number;
+
+  if (nobodyWon) {
+    casualtiesBase = 1;
+  } else {
+    casualtiesBase = attackerWon
+      ? defenderPoints / attackerPoints
+      : attackerPoints / defenderPoints;
+  }
+
+  const casualtiesRatio = casualtiesBase ** (3 / 2);
+
+  // Step 3: compute casualties for each team
+  let attackerCasualties;
+  let defenderCasualties;
+
+  if (nobodyWon) {
+    attackerCasualties = computeCasualtiesForTeam(attackerUnitsInfoByType, 1);
+    defenderCasualties = computeCasualtiesForTeam(defenderUnitsInfoByType, 1);
+  } else {
+    attackerCasualties = computeCasualtiesForTeam(
+      attackerUnitsInfoByType,
+      attackerWon ? casualtiesRatio : 1,
+    );
+    defenderCasualties = computeCasualtiesForTeam(
+      attackerUnitsInfoByType,
+      attackerWon ? 1 : casualtiesRatio,
+    );
+  }
+
+  // Step 4, format the Report
+  return {
+    attackId,
+    travelTime,
+    attackerVillageId,
+    defenderVillageId,
+    winnerVillageId: attackerWon ? attackerVillageId : defenderVillageId,
+    loserVillageId: attackerWon ? defenderVillageId : attackerVillageId,
+    unitsInfoByType: {
+      [attackerVillageId]: attackerUnitsInfoByType,
+      [defenderVillageId]: defenderUnitsInfoByType,
+    },
+    casualties: {
+      [attackerVillageId]: {
+        casualtiesInfoByUnitTypeId: attackerCasualties,
+      },
+      [defenderVillageId]: {
+        casualtiesInfoByUnitTypeId: defenderCasualties,
+      },
+    },
+  };
+};

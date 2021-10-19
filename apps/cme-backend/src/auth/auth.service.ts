9620b5dd710e4681324f995d849afd0dc7ff91a8
Florian Eyzat <florianeyzat1993@gmail.com>
2021-10-19 18:42:08 +0200

    Build/TS updates

--- apps/cme-backend/src/auth/auth.service.ts
+++ apps/cme-backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import * as _ from 'lodash';
+import _ from 'lodash';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
@@ -12,10 +12,10 @@ export class AuthService {
     private readonly jwtService: JwtService
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<User> {
+  async validateUser(username: string, pass: string): Promise<Partial<User>> {
     const user = await this.userRepository.findOneByUsername(username);
     if (user && (await bcrypt.compare(pass, user.password)))
-      return _.omit(user, 'password');  // todo: use model's @Exclude() decorator
+      return _.omit(user, 'password');
   }
 
   async login(user: User) {

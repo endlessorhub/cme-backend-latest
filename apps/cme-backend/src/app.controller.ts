ae3772b7510939bfd5bbc8a4fd16cdc85caccb48
endlessorhub <anthonyhutchi99@gmail.com>
2021-11-07 17:25:37 +0100

    Updating just to try

--- apps/cme-backend/src/app.controller.ts
+++ apps/cme-backend/src/app.controller.ts
@@ -4,7 +4,7 @@ import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { Public } from './public.decorator';
 import { LoginDto } from './auth/dto/login.dto';
-import { ApiBody } from '@nestjs/swagger';
+import { ApiBody, ApiProperty } from '@nestjs/swagger';
 
 
 @Controller()
@@ -14,6 +14,10 @@ export class AppController {
   @Public()
   @ApiBody({ type: LoginDto })
   @UseGuards(LocalAuthGuard)
+  @ApiProperty({
+    description: `Basic Auth`,
+    example: ['admin/admin'],
+  })
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);

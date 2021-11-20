9200f34adbfacbad35a2e9d1d22cc8b46abee668
Simon Legroux <simon.legroux@meetbarney.fr>
2021-11-20 16:08:58 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,23 @@
 #Should be executed as sudo and be downloaded apart
 
+echo "Deleting previous repo"
+rm -r /home/gitlab-ci/cme-backend
 echo "Cloning Repo"
 mkdir /home/gitlab-ci
 cd /home/gitlab-ci
-rm -r /home/gitlab-ci/cme-backend
 git clone https://gitlab+deploy-token-635521:PSUbjHSvT4s9DYBkS4Pm@gitlab.com/samy.f/cme-backend.git
+chown -R admin:admin cme-backend
 cd cme-backend
 
-echo "Pulling Repo"
-git pull https://gitlab+deploy-token-635521:PSUbjHSvT4s9DYBkS4Pm@gitlab.com/samy.f/cme-backend.git
+#echo "Pulling Repo"
+#git pull https://gitlab+deploy-token-635521:PSUbjHSvT4s9DYBkS4Pm@gitlab.com/samy.f/cme-backend.git
 
 echo "Making executables"
 chmod u+x /home/gitlab-ci/cme-backend/init.sh
 chmod u+x /home/gitlab-ci/cme-backend/boot.sh
 
 echo "Building dockers"
-sudo ./init.sh
+./init.sh
 
 echo "Booting dockers"
-sudo ./boot.sh
+./boot.sh

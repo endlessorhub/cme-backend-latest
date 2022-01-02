861a6458e9a077a77511bb38327b3dc9bafa02e2
endlessorhub <anthonyhutchi99@gmail.com>
2022-01-02 16:26:26 +0100

    Small code clean

--- apps/cme-backend/src/app.controller.ts
+++ apps/cme-backend/src/app.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { Public } from './public.decorator';

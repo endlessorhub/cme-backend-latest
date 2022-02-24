37ddd16403f9408550cd12987f96eb66224cfe70
Florian Eyzat <florianeyzat1993@gmail.com>
2022-02-24 22:45:45 +0100

    Add base microservice commands for later

--- apps/resources-ms/src/service-messages.ts
+++ apps/resources-ms/src/service-messages.ts
@@ -2,4 +2,10 @@ export const ResourcesMicroServiceName = 'RESOURCES_MS';
 
 export const ResourcesMicroServiceMessages = {
   TEST_SERVICE: `${ResourcesMicroServiceName}test_service`,
+  CREATE_FACILITY: `${ResourcesMicroServiceName}_create_facility`,
+  FIND_FACILITY: `${ResourcesMicroServiceName}_find_facility`,
+  GET_VILLAGE_FACILITIES: `${ResourcesMicroServiceName}_get_village_facilities`,
+  CREATE_ORDER: `${ResourcesMicroServiceName}_create_order`,
+  GET_VILLAGE_RESOURCES: `${ResourcesMicroServiceName}_get_village_resources`,
+  MERGE_UNIT_RULES: `${ResourcesMicroServiceName}_merge_unit_rules`,
 };

f9d82bf4e316ee14a33b50cd64fca5e0e0b62e4e
Simon Legroux <simon.legroux@gmail.com>
2022-02-24 12:32:18 +0000

    Add email as mandatory in register route

---
+++ apps/cme-backend/src/mail/mail.service.ts
@@ -0,0 +1,59 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { User } from './../users/user.entity';
+import { Logger } from '@nestjs/common';
+import { UserRepository } from '../users/user.repository';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  private logger: Logger = new Logger('AppController');
+  async sendUserConfirmation(user: User, token: string) {
+    const url = `example.com/auth/confirm?token=${token}`;
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      // from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to Nice App! Confirm your Email',
+      template: './confirmation', // `.hbs` extension is appended automatically
+      context: { // ✏️ filling curly brackets with content
+        name: user.email,
+        url : url
+      },
+    });
+  }
+
+  async sendAttemptLoginEmail(email) {
+    
+    this.logger.log(`Sending email to => ${email}`);
+    await this.mailerService.sendMail({
+      to: email,
+      // from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to MonkeyEmpire',
+      template: './apps/cme-backend/src/mail/templates/login_attempt_notification', // `.hbs` extension is appended automatically
+      context: { // ✏️ filling curly brackets with content
+        name: email,
+        date : new Date(Date.now())
+      },
+    });
+  }
+
+  async sendVerificationEmail(user) {
+    
+    this.logger.log(`Sending email to => ${user.username} for verification with link ${user.email_verification_token}`);
+    
+    await this.mailerService.sendMail({
+      to: user.username,
+      // from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Email verification for MonkeyEmpire',
+      template: './apps/cme-backend/src/mail/templates/email_verification_message', // `.hbs` extension is appended automatically
+      context: { // ✏️ filling curly brackets with content
+        name: user.username,
+        link : user.email_verification_token,
+        date : new Date(Date.now())
+      },
+    });
+  }
+
+}

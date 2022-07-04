2844bb8ffee39cb023cc722be5577821d53d6cce
Simon Legroux <simon.legroux@gmail.com>
2022-07-04 17:27:00 +0000

    Add scientist and warlike indicators

--- apps/cme-backend/src/public/public.controller.ts
+++ apps/cme-backend/src/public/public.controller.ts
@@ -18,4 +18,25 @@ export class PublicController {
   async leaders(@Request() req, @Query() queryParams: GetLeaders) {
     return await this.publicService.getVillageLeaders(queryParams.limit);
   }
+
+  @Public()
+  @Get('leaders/warlike')
+  async warlike(@Request() req, @Query() queryParams: GetLeaders) {
+    return await this.publicService.getVillageLeadersWarLike(queryParams.limit);
+  }
+
+  @Public()
+  @Get('leaders/scientist')
+  async scientist(@Request() req, @Query() queryParams: GetLeaders) {
+    return await this.publicService.getVillageScientist(queryParams.limit);
+  }
+
+
+  @Public()
+  @Get('leaders/biggestStealer')
+  async biggestStealer(@Request() req, @Query() queryParams: GetLeaders) {
+    return await this.publicService.getBiggestStealer(queryParams.limit);
+  }
+
+
 }

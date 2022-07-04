e0792c201e7ab5582652a9adefec80f1b56a223e
Simon Legroux <simon.legroux@gmail.com>
2022-07-04 17:27:00 +0000

    Add scientist and warlike indicators

--- apps/cme-backend/src/public/public.service.ts
+++ apps/cme-backend/src/public/public.service.ts
@@ -45,4 +45,105 @@ export class PublicService {
 
     return rows;
   }
+
+  async getVillageLeadersWarLike(
+    limit: number = DEFAULT_LIMIT_LEADERS,
+  ): Promise<ReadonlyArray<VillageLeader>> {
+    let rows = [];
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const limitChecked = limit <= MAX_LIMIT_LEADERS ? limit : MAX_LIMIT_LEADERS;
+
+    try {
+      rows = await queryRunner.query(`
+        SELECT users.username, count(*) as attack_count
+          FROM attacks
+        LEFT JOIN 
+            users on users.id = attacker_id group by users.username
+        order by count_attack desc 
+        LIMIT ${limitChecked}
+      `);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      console.error(err);
+    } finally {
+      await queryRunner.release();
+    }
+
+    return rows;
+  }
+
+  async getVillageScientist(
+    limit: number = DEFAULT_LIMIT_LEADERS,
+  ): Promise<ReadonlyArray<VillageLeader>> {
+    let rows = [];
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const limitChecked = limit <= MAX_LIMIT_LEADERS ? limit : MAX_LIMIT_LEADERS;
+
+    try {
+      rows = await queryRunner.query(`
+        SELECT village_id, sum(level) as tech_level, name, username 
+          FROM facilities
+        left join users 
+        left join villages 
+        on villages.user_id = users.id 
+        on village_id = villages.id 
+        group by village_id, villages.name, users.username 
+        order by sum desc
+        LIMIT ${limitChecked}
+      `);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      console.error(err);
+    } finally {
+      await queryRunner.release();
+    }
+
+    return rows;
+  }
+
+  async getBiggestStealer(
+    limit: number = DEFAULT_LIMIT_LEADERS,
+  ): Promise<ReadonlyArray<VillageLeader>> {
+    let rows = [];
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const limitChecked = limit <= MAX_LIMIT_LEADERS ? limit : MAX_LIMIT_LEADERS;
+
+    try {
+      rows = await queryRunner.query(`
+      select count(*) as stolen_volume, attacker_id, username
+        from (select json_array_elements(resources_stolen)->'count' as stolen_resources, attacker_id 
+          from (select stolen_resources->'resources' as resources_stolen, attacker_id from attacks where stolen_resources is not NULL) 
+            as x) 
+              as y 
+                left join users on users.id = attacker_id 
+      group by attacker_id, username order by stolen_volume desc 
+      LIMIT ${limitChecked}
+      `);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      console.error(err);
+    } finally {
+      await queryRunner.release();
+    }
+
+    return rows;
+  }
+
+
+
 }

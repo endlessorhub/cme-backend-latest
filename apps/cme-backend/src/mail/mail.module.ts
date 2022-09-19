75629d3999b71db6e338d1b334e0ad5ef8734f22
igoriev26 <igoryev26@gmail.com>
2022-09-19 15:02:50 -0400

    merge conflicts

---
+++ apps/cme-backend/src/mail/mail.module.ts
@@ -0,0 +1,35 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: "smtp.gmail.com",
+        port: "465",
+        secure: true,
+        auth: {
+          user: 'support@monkeyempire.net',
+          pass: '469JzN49$469JzN49$',
+        },
+      },
+      defaults: {
+        from: '"Monkey Empire" <noreply@monkeyempire.net>',
+        replyTo : "support@monkeyempire.net"
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService], // 👈 export for DI
+})
+export class MailModule {}
--- apps/cme-backend/src/scripts/generate-ormconfig.ts
+++ apps/cme-backend/src/scripts/generate-ormconfig.ts
@@ -7,7 +7,8 @@ const YAML_CONFIG_FILENAME = `${process.env.NODE_ENV}.yml`;
 const configPath = path.join(__dirname, '..', '..', 'config', YAML_CONFIG_FILENAME);
 const ormConfigPath = path.join(__dirname, '..', '..', '..', '..', 'ormconfig.json');
 
-const config = yaml.load(
+export const config = yaml.load(
     fs.readFileSync(configPath, 'utf8'),
 ) as any;
+
 fs.writeFileSync(ormConfigPath, JSON.stringify(config.typeorm, null, 2));

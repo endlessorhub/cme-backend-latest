12c80f38936150a5763cd88439cdc78237fb3a7a
mudassirkhan007 <just00seven@gmail.com>
2022-09-21 10:36:35 +0500

    wallet generation added in microservice

--- apps/blockchain-ms/src/services/blockchain-ms-ingame-mkc.service.ts
+++ apps/blockchain-ms/src/services/blockchain-ms-ingame-mkc.service.ts
@@ -6,7 +6,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { isEmpty } from 'lodash';
 import { UserGlobalMKC } from 'apps/cme-backend/src/user-global-mkc/user-global-mkc.entity';
-
+const HDWallet = require('ethereum-hdwallet');
+const hdwallet = HDWallet.fromMnemonic(
+  'tag volcano eight thank tide danger coast health above argue embrace heavy',
+);
 @Injectable()
 export class BlockchainMsIngameMKCService {
   private logger: Logger = new Logger('BlockchainMsIngameMKCService');
@@ -32,6 +35,24 @@ export class BlockchainMsIngameMKCService {
 
     return {
       balance: balanceOrEmpty?.balance | 0,
+    };
+  }
+
+  async getUserWallet(derive: number): Promise<any> {
+    const mywallet = hdwallet.derive(`m/44'/60'/0'/0`);
+    const walletAddress = `0x${mywallet
+      .derive(derive)
+      .getAddress()
+      .toString('hex')}`;
+
+    const privateKey = `${mywallet
+      .derive(derive)
+      .getPrivateKey()
+      .toString('hex')}`;
+
+    return {
+      walletAddress,
+      privateKey,
     };
   }
 }
